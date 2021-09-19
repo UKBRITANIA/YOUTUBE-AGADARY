9936906f8ed76189744968032d75eddf37603386
UKBRITANIA <90155366+UKBRITANIA@users.noreply.github.com>
2021-09-19 22:36:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "",
+    token: "ODYxOTUzMzU4NzAwMDg1Mjg5.YORSnw.wsXn7t4RkQ_TuOlq9aYNW44Z6hI",
     channel: "833853700803264555",
     messageTemplate: "Slaw @everyone, **{author}** Vedio nwe bllawkrayawa✅ **{title}**!\n{url}",
     channel_id: "UCFbWvTkRe8Fq2aXmnIvLp9A",
